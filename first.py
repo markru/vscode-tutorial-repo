c24d978bac13daebad8c754acf2a7f6739cde3a7
markru <this.markru@gmail.com>
2020-04-11 17:19:18 -0700

    cleanup first

--- first.py
+++ first.py
@@ -1,15 +1,11 @@
-import math
 import sys
-from os import rename
 
 import pip._vendor.requests as requests
 
-# print(sys.version)
 print(sys.executable)
 
 
 def greet(who_to_greet):
-    test = "Test"
     greeting = "Hello, {}".format(who_to_greet)
     return greeting
 
